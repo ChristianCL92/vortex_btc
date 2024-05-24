2bb3a3c95c8dc972e9470f7ef283daef14fafc51
Christian Lopez <chrisdev92@outlook.com>
2024-05-24 15:47:57 +0200

    imported block-router file to server

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import blockchainRouter from "./routes/blockchain-routes.mjs"
+import blockRouter from "./routes/block-routes.mjs"
 
 const app = express();
 
 app.use("/api/v1/blockchain", blockchainRouter)
+app.use("/api/v1/block", blockRouter)
 
 const PORT = 4001;
 
