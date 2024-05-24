864c8ecd6d37bc90fa6f4c02d01263d6509cd4d5
Hedi Gardi <hedigardi@hotmail.com>
2024-05-24 09:12:31 +0200

    added fetchBlockchain example

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Blockchain from './components/Blockchain';
 import AddTransaction from './components/AddTransaction';
 import './App.css';
 
 function App() {
+  // const [blockchain, setBlockchain] = useState([]);
   const [message, setMessage] = useState('');
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // fetchBlockchain();
+  }, []);
+
+  /* 
+  const fetchBlockchain = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:5001/api/v1/<placeholder>'
+      );
+      setBlockchain(response.data.data.chain);
+    } catch (error) {
+      console.error('Error fetching blockchain', error);
+    }
+  }; 
+  */
 
   const handleAddTransaction = async (transaction) => {
     try {
@@ -15,6 +32,7 @@ function App() {
         transaction
       );
       setMessage(response.data.data.message);
+      // fetchBlockchain();
     } catch (error) {
       console.error('Error adding transaction', error);
     }
@@ -23,6 +41,7 @@ function App() {
   return (
     <div>
       <h1>Blockchain Explorer</h1>
+      {/* <Blockchain blockchain={blockchain} /> */}
       <AddTransaction onAddTransaction={handleAddTransaction} />
       {message && <p>{message}</p>}
     </div>
