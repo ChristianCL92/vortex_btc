ee5b91eb884bc35649a2e2d27cc8fa8199e60a12
Hedi Gardi <hedigardi@hotmail.com>
2024-05-24 08:57:38 +0200

    added AddTransaction function

---
+++ frontend/src/components/AddTransaction.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const AddTransaction = ({ onAddTransaction }) => {
+  const [amount, setAmount] = useState('');
+  const [sender, setSender] = useState('');
+  const [recipient, setRecipient] = useState('');
+
+  return <div></div>;
+};
+
+export default AddTransaction;
