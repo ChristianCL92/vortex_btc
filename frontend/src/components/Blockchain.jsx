7d851cfedd712c4ea45dac512e61876308cba1d8
Hedi Gardi <hedigardi@hotmail.com>
2024-05-28 20:49:36 +0200

    updated Blockchain component and styling

--- frontend/src/components/Blockchain.jsx
+++ frontend/src/components/Blockchain.jsx
@@ -11,30 +11,40 @@ const Blockchain = ({ blockchain }) => {
             <span className="label">Block</span> {block.blockIndex}
           </h3>
           <p>
-            <span className="label">Timestamp:</span>{' '}
-            {block.timestamp && new Date(block.timestamp).toLocaleString()}
+            <span className="label">Timestamp:</span>
+            <span>
+              {block.timestamp && new Date(block.timestamp).toLocaleString()}
+            </span>
           </p>
           <p>
-            <span className="label">Previous Hash:</span>{' '}
-            {block.previousBlockHash}
+            <span className="label">Previous Block Hash:</span>
+            <span>{block.previousBlockHash}</span>
           </p>
           <p>
-            <span className="label">Hash:</span> {block.currentBlockHash}
+            <span className="label">Current Block Hash:</span>
+            <span>{block.currentBlockHash}</span>
           </p>
           <p>
-            <span className="label">Nonce:</span> {block.nonce}
+            <span className="label">Nonce:</span>
+            <span>{block.nonce}</span>
+          </p>
+          <p>
+            <span className="label">Difficulty:</span>
+            <span>{block.difficulty}</span>
           </p>
           <p>
             <span className="label">Transactions:</span>
-            {block.data && block.data.length > 0 ? (
-              block.data.map((tx, txIndex) => (
-                <span key={txIndex}>
-                  {` ID: ${tx.transactionId}, Sender: ${tx.sender}, Recipient: ${tx.recipient}, Amount: ${tx.amount}`}
-                </span>
-              ))
-            ) : (
-              <span> No transactions</span>
-            )}
+            <span>
+              {block.data && block.data.length > 0 ? (
+                block.data.map((tx, txIndex) => (
+                  <span key={txIndex}>
+                    {` ID: ${tx.transactionId}, Sender: ${tx.sender}, Recipient: ${tx.recipient}, Amount: ${tx.amount}`}
+                  </span>
+                ))
+              ) : (
+                <span>No transactions</span>
+              )}
+            </span>
           </p>
         </div>
       ))}
